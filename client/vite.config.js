505d3b5d314fedaf3326afcced84337c62db52d6
robdeer1 <r_deer@fanshaweonline.ca>
2024-12-14 09:40:00 -0500

    updated vite.config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/", // Ensure this is correct ("/" for root hosting or a subpath like "/mysimpleblogpage/" if needed)
-  build: {
-    outDir: "dist",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
   },
+  plugins: [react()],
 });
