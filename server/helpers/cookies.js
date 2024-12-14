33d817c656f25937c5ed7cbdedff7e049970441a
robdeer1 <r_deer@fanshaweonline.ca>
2024-12-14 13:44:00 -0500

    updated cookie options

--- server/helpers/cookies.js
+++ server/helpers/cookies.js
@@ -1,6 +1,6 @@
 exports.cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production", // Secure only in production
-  sameSite: "strick",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Cross-origin support in production
   maxAge: 24 * 60 * 60 * 1000, // Token expires in 1 day
 };
